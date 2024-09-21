bcc6a0c34eb9932ea85eedd487a3ae40c159d1d1
Mohini Ojha <mohini.17aug@gmail.com>
2024-09-21 22:23:56 +0530

    added script to add super user in apps.py: import OS

--- timesheet/apps.py
+++ timesheet/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
+import os
 
 class TimesheetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
